4cc0b8ddbcf37859245118f9f806088d95c1a743
Roger45130 <rogerlg@netcourrier.com>
2025-02-24 16:41:40 +0100

    MaJ 24022025 1640 Header du dashboard

--- admin/index.php
+++ admin/index.php
@@ -14,7 +14,7 @@ require_once('include/header.php');
                         <span class="navbar-toggler-bar bar3"></span>
                     </button>
                 </div>
-                <a class="navbar-brand" href="javascript:;">Paper Dashboard 2</a>
+                <a class="navbar-brand" href="javascript:;">Dashboard / Admin</a>
             </div>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-bar navbar-kebab"></span>
