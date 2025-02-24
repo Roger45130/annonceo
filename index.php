6f0f3ed1b8c4a8d327e857911de95bced6e503ac
Roger45130 <rogerlg@netcourrier.com>
2025-02-24 14:58:59 +0100

    MaJ 24022025 1500 Index-Header-Footer Front & Back

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require_once('include/header.php');
+?>
+
+<div class="content">
+    <div class="row">
+        <div class="col-md-12">
+            <h3 class="description">Your content here</h3>
+        </div>
+    </div>
+</div>
+<?php
+require_once('include/footer.php');
+?>
